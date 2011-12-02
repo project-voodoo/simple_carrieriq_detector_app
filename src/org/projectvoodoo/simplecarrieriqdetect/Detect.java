82907020ca03218895a158200add2e95215d9311
SIMOND François <francois@lienweb.fr>
2011-12-02 03:47:52 +0100

    app skel

---
+++ src/org/projectvoodoo/simplecarrieriqdetect/Detect.java
@@ -0,0 +1,15 @@
+
+package org.projectvoodoo.simplecarrieriqdetect;
+
+import android.content.Context;
+
+public class Detect {
+
+    Context context;
+
+    public Detect(Context context) {
+        this.context = context;
+
+    }
+
+}
